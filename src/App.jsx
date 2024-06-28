85afbdb64665b78bb53d26b3a4e4e684a9f4ab4a
krishna1621 <115209389+krishna1621@users.noreply.github.com>
2024-06-28 11:37:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import ContactPage from "./Components/ContactPage/ContactPage";
+import Home from "./Components/HomePage/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PlatformPage from "./Components/PlatformPage/PlatformPage";
+import SolutionAi from "./Components/SolutionAI/SolutionAi";
+import ShodatAI from "./Components/ShodatAI/ShodatAI";
+import About from "./Components/About/About";
+import Navbar from "./Components/Navbar1/Navbar";
+import ScrollToTop from "../ScrollToTop";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <ScrollToTop />
+        <div className="md:flex md:justify-center">
+          <Navbar id="navbar" />
+        </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/platform" element={<PlatformPage />} />
+            <Route path="/solutionAI" element={<SolutionAi />} />
+            <Route path="/shodatAI" element={<ShodatAI />} />
+            <Route path="/about" element={<About />} />
+            {/* Add more routes for additional pages */}
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
